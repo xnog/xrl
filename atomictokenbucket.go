dd30974ae1108633611c6385df1c1ac67571cb58
Felipe Nogueira Gaudio <xnogueira@gmail.com>
2024-02-04 00:26:52 +0100

    split token bucket into normal and atomic

--- atomictokenbucket.go
+++ atomictokenbucket.go
@@ -7,7 +7,7 @@ import (
 	"github.com/redis/go-redis/v9"
 )
 
-type TokenBucketRateLimiter struct {
+type AtomicTokenBucketRateLimiter struct {
 	client *redis.Client
 	script *redis.Script
 	keys   []string
@@ -24,7 +24,7 @@ type TokenBucketRateLimiter struct {
 // For 1333 requests per second with maximum burst of 1500 requests per second
 // Capacity: 1500
 // Rate: 1333
-func NewTokenBucketRateLimiter(client *redis.Client, key string, capacity int, rate float64) *TokenBucketRateLimiter {
+func NewAtomicTokenBucketRateLimiter(client *redis.Client, key string, capacity int, rate float64) *AtomicTokenBucketRateLimiter {
 	keys := []string{key}
 	args := []interface{}{capacity, rate}
 
@@ -33,26 +33,28 @@ func NewTokenBucketRateLimiter(client *redis.Client, key string, capacity int, r
 		local capacity = tonumber(ARGV[1])
 		local rate = tonumber(ARGV[2])
 
-		local values = redis.call("mget", key, key .. ":timestamp")
+		local values = redis.call("mget", key, key .. ":timestamp", key .. ":process")
 		local tokens = tonumber(values[1] or 0)
 		local timestamp = tonumber(values[2] or 0)
+		local process = tonumber(values[3] or 0)
 
 		local now = redis.call("time")[1]
 		local elapsed = now - timestamp
 
-		tokens = math.min(capacity, tokens + elapsed * rate)
+		tokens = math.min(capacity - process, tokens + elapsed * rate)
 
 		redis.call("mset", key, tokens, key .. ":timestamp", now)
 
 		if tokens >= 1 then
 			redis.call("incrbyfloat", key, -1)
+			redis.call("setex", key .. ":process", 60, process + 1)
 			return 0
 		else
 			return 1
 		end
 	`)
 
-	return &TokenBucketRateLimiter{
+	return &AtomicTokenBucketRateLimiter{
 		client,
 		script,
 		keys,
@@ -60,7 +62,7 @@ func NewTokenBucketRateLimiter(client *redis.Client, key string, capacity int, r
 	}
 }
 
-func (r *TokenBucketRateLimiter) Wait(ctx context.Context, wait time.Duration) error {
+func (r *AtomicTokenBucketRateLimiter) Take(ctx context.Context, wait time.Duration) error {
 	for {
 		l, err := r.script.Run(ctx, r.client, r.keys, r.args...).Int()
 		if err != nil {
@@ -75,4 +77,9 @@ func (r *TokenBucketRateLimiter) Wait(ctx context.Context, wait time.Duration) e
 	}
 
 	return nil
+}
+
+func (r *AtomicTokenBucketRateLimiter) Put(ctx context.Context) error {
+	_, err := r.client.Decr(ctx, r.keys[0]+":process").Result()
+	return err
 }
